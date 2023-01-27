0d8b14646e6ab73cdc19082f12bea66af5d2103f
hoc081098 <hoc081098@gmail.com>
2023-01-27 15:53:12 +0700

    detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,26 @@
 import com.diffplug.gradle.spotless.SpotlessExtension
 import com.diffplug.gradle.spotless.SpotlessPlugin
+import io.gitlab.arturbosch.detekt.DetektPlugin
+import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 
 plugins {
   kotlin("multiplatform") version "1.7.21" apply false
   id("com.android.library") version "7.3.0" apply false
   id("com.diffplug.gradle.spotless") version "6.14.0" apply false
+  id("io.gitlab.arturbosch.detekt") version "1.22.0" apply false
 }
 
 val ktlintVersion = "0.48.2"
+
+subprojects {
+  apply<DetektPlugin>()
+  configure<DetektExtension> {
+    source = files("src/")
+    config = files("${project.rootDir}/detekt.yml")
+    buildUponDefaultConfig = true
+    allRules = true
+  }
+}
 
 allprojects {
   apply<SpotlessPlugin>()
