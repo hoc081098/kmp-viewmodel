08b74e5f3460467b4ce1404181917c442e583c65
hoc081098 <hoc081098@gmail.com>
2023-02-11 15:52:15 +0700

    ignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,15 @@ import com.diffplug.gradle.spotless.SpotlessExtension
 import com.diffplug.gradle.spotless.SpotlessPlugin
 import io.gitlab.arturbosch.detekt.DetektPlugin
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+import kotlinx.kover.KoverPlugin
+import kotlinx.kover.api.KoverMergedConfig
 
 plugins {
   kotlin("multiplatform") version "1.7.21" apply false
   id("com.android.library") version "7.3.0" apply false
   id("com.diffplug.gradle.spotless") version "6.14.0" apply false
   id("io.gitlab.arturbosch.detekt") version "1.22.0" apply false
+  id("org.jetbrains.kotlinx.kover") version "0.6.1" apply false
 }
 
 val ktlintVersion = "0.48.2"
@@ -23,6 +26,11 @@ subprojects {
 }
 
 allprojects {
+  apply<KoverPlugin>()
+  configure<KoverMergedConfig> {
+    enable()
+  }
+
   apply<SpotlessPlugin>()
   configure<SpotlessExtension> {
     kotlin {
