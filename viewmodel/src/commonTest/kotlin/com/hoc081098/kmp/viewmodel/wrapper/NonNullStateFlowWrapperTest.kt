8b2a3b5af8df8f12739b4bb45f8981c4a256e56d
hoc081098 <hoc081098@gmail.com>
2023-04-01 23:42:57 +0700

    basic test

---
+++ viewmodel/src/commonTest/kotlin/com/hoc081098/kmp/viewmodel/wrapper/NonNullStateFlowWrapperTest.kt
@@ -0,0 +1,30 @@
+package com.hoc081098.kmp.viewmodel.wrapper
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+
+class NonNullStateFlowWrapperTest {
+  @Test
+  fun basic() = runTest {
+    val stateFlow = MutableStateFlow(1)
+    val flowWrapper = stateFlow.wrap()
+
+    val values = mutableListOf<Int>()
+    val job = launch(context = UnconfinedTestDispatcher(testScheduler)) {
+      flowWrapper.toList(values)
+    }
+
+    stateFlow.value = 2
+    stateFlow.value = 3
+    stateFlow.value = 4
+
+    job.cancelAndJoin()
+    assertEquals(expected = listOf(1, 2, 3, 4), actual = values)
+  }
+}
