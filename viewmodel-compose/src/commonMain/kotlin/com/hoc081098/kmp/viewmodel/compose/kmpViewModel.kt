b13f1473dda91cf45b599b9162fafa684dcbbfc2
hoc081098 <hoc081098@gmail.com>
2023-08-07 00:59:45 +0700

    add

--- viewmodel-compose/src/commonMain/kotlin/com/hoc081098/kmp/viewmodel/compose/kmpViewModel.kt
+++ viewmodel-compose/src/commonMain/kotlin/com/hoc081098/kmp/viewmodel/compose/kmpViewModel.kt
@@ -1,6 +1,7 @@
 package com.hoc081098.kmp.viewmodel.compose
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisallowComposableCalls
 import com.hoc081098.kmp.viewmodel.ViewModel
 
 public interface ViewModelFactory<VM : ViewModel> {
@@ -18,3 +19,12 @@ public expect inline fun <reified VM : ViewModel> kmpViewModel(
   key: String? = null,
   factory: ViewModelFactory<VM>,
 ): VM
+
+@Composable
+public inline fun <reified VM : ViewModel> kmpViewModel(
+  key: String? = null,
+  crossinline factory: @DisallowComposableCalls () -> VM,
+): VM = kmpViewModel(
+  key = key,
+  factory = viewModelFactory(factory),
+)
