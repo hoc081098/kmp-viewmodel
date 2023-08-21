f47db3352f3568b60c21cfe3ebf9cf2069479596
hoc081098 <hoc081098@gmail.com>
2023-08-22 03:48:13 +0700

    LocalViewModelStoreOwner.kt

--- viewmodel-compose/src/commonMain/kotlin/com/hoc081098/kmp/viewmodel/compose/kmpViewModel.kt
+++ viewmodel-compose/src/commonMain/kotlin/com/hoc081098/kmp/viewmodel/compose/kmpViewModel.kt
@@ -2,26 +2,18 @@ package com.hoc081098.kmp.viewmodel.compose
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisallowComposableCalls
-import androidx.compose.runtime.Stable
 import com.hoc081098.kmp.viewmodel.CreationExtras
 import com.hoc081098.kmp.viewmodel.MainThread
 import com.hoc081098.kmp.viewmodel.ViewModel
 import com.hoc081098.kmp.viewmodel.ViewModelFactory
 import com.hoc081098.kmp.viewmodel.ViewModelStoreOwner
 
-@Stable
-public expect fun defaultPlatformCreationExtras(): CreationExtras
-
-@MainThread
-@Composable
-public expect fun defaultPlatformViewModelStoreOwner(): ViewModelStoreOwner
-
 @MainThread
 @Composable
 public expect inline fun <reified VM : ViewModel> kmpViewModel(
   key: String? = null,
   extras: CreationExtras = defaultPlatformCreationExtras(),
-  viewModelStoreOwner: ViewModelStoreOwner = defaultPlatformViewModelStoreOwner(),
+  viewModelStoreOwner: ViewModelStoreOwner = defaultViewModelStoreOwner(),
   factory: ViewModelFactory<VM>,
 ): VM
 
@@ -29,7 +21,7 @@ public expect inline fun <reified VM : ViewModel> kmpViewModel(
 @Composable
 public inline fun <reified VM : ViewModel> kmpViewModel(
   key: String? = null,
-  viewModelStoreOwner: ViewModelStoreOwner = defaultPlatformViewModelStoreOwner(),
+  viewModelStoreOwner: ViewModelStoreOwner = defaultViewModelStoreOwner(),
   extras: CreationExtras = defaultPlatformCreationExtras(),
   crossinline factory: @DisallowComposableCalls CreationExtras.() -> VM,
 ): VM = kmpViewModel(
