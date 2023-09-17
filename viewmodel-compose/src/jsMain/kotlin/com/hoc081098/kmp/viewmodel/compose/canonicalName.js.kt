ba4c87ef5ba1e17ac20c929af5bd0f23e62a5a3d
hoc081098 <hoc081098@gmail.com>
2023-09-17 20:13:49 +0700

    compose: add @JvmSynthetic to some methods

--- viewmodel-compose/src/jsMain/kotlin/com/hoc081098/kmp/viewmodel/compose/canonicalName.js.kt
+++ viewmodel-compose/src/jsMain/kotlin/com/hoc081098/kmp/viewmodel/compose/canonicalName.js.kt
@@ -2,6 +2,5 @@ package com.hoc081098.kmp.viewmodel.compose
 
 import kotlin.reflect.KClass
 
-@PublishedApi
 internal actual inline val <T : Any> KClass<T>.canonicalName: String?
   get() = simpleName
