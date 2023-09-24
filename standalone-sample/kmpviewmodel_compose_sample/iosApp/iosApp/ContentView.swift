fb74f28ee66417b57aba0ced03ef283e9f52eaa6
hoc081098 <hoc081098@gmail.com>
2023-09-24 18:34:50 +0700

    create ios app

---
+++ standalone-sample/kmpviewmodel_compose_sample/iosApp/iosApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iosApp
+//
+//  Created by Hoc Nguyen T. on 9/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
