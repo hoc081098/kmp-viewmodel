5c90a252fffd7f6a7d3d54d98f5a3964b218dce9
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2023-12-08 19:25:18 +0700

    Update update_docs_url.sh: `docs/`

--- scripts/update_docs_url.sh
+++ scripts/update_docs_url.sh
@@ -5,7 +5,7 @@ input=$1
 
 if [[ "$input" == "snapshot" ]]; then
     brew install gnu-sed
-    gsed -i 's/]: https:\/\/hoc081098.github.io\/kmp-viewmodel\/docs\/0.x/]: https:\/\/hoc081098.github.io\/kmp-viewmodel\/docs\/latest/g' index.md
+    gsed -i 's/]: https:\/\/hoc081098.github.io\/kmp-viewmodel\/docs\/0.x/]: https:\/\/hoc081098.github.io\/kmp-viewmodel\/docs\/latest/g' docs/index.md
     echo "Updated index.md file with snapshot version."
 else
     echo "Invalid input: $input"
