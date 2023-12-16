fa69520e17106f9fa6773f6d0ee393d86a316652
hoc081098 <hoc081098@gmail.com>
2023-12-16 15:10:01 +0700

    dump

--- viewmodel-savedstate/src/androidMain/kotlin/com/hoc081098/kmp/viewmodel/SafeSavedStateHandle.android.kt
+++ viewmodel-savedstate/src/androidMain/kotlin/com/hoc081098/kmp/viewmodel/SafeSavedStateHandle.android.kt
@@ -3,11 +3,11 @@ package com.hoc081098.kmp.viewmodel
 import androidx.lifecycle.LiveData
 
 public fun <T : Any> SafeSavedStateHandle.getLiveData(key: SavedStateHandleKey<T>): LiveData<T> =
-  this.savedStateHandle
+  savedStateHandle
     .getLiveData(key.key, key.defaultValue)
     .also { check(it.isInitialized) { "LiveData isn't initialized" } }
 
 public fun <T : Any> SafeSavedStateHandle.getLiveData(key: NullableSavedStateHandleKey<T>): LiveData<T?> =
-  this.savedStateHandle
+  savedStateHandle
     .getLiveData(key.key, key.defaultValue)
     .also { check(it.isInitialized) { "LiveData isn't initialized" } }
