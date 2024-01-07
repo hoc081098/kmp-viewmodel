bdd0e9e944c0272d9980f99194f88701db13669b
hoc081098 <hoc081098@gmail.com>
2024-01-07 17:20:19 +0700

    NonNullSavedStateHandleKey  https://github.com/androidx/androidx/commit/2ffce096e13e3aa4675a8b0fd8b0d74cb1ced653

--- sample/app/src/main/java/com/hoc081098/kmpviewmodelsample/android/StartViewModel.kt
+++ sample/app/src/main/java/com/hoc081098/kmpviewmodelsample/android/StartViewModel.kt
@@ -4,6 +4,7 @@ import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.hoc081098.kmp.viewmodel.safe.NonNullSavedStateHandleKey
 import com.hoc081098.kmp.viewmodel.safe.NullableSavedStateHandleKey
 import com.hoc081098.kmp.viewmodel.safe.parcelableArray
 import com.hoc081098.kmp.viewmodel.safe.safe
@@ -18,38 +19,71 @@ internal data class TestParcelable(val value: Long) : Parcelable
 internal class StartViewModel(
   private val savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
-  private val stateFlow = savedStateHandle.safe { it.getStateFlow(PARCELABLE_ARRAY_KEY) }
+  private val nullableStateFlow = savedStateHandle.safe { it.getStateFlow(NULLABLE_PARCELABLE_ARRAY_KEY) }
+  private val nonNullStateFlow = savedStateHandle.safe { it.getStateFlow(NON_NULL_PARCELABLE_ARRAY_KEY) }
 
   init {
     Napier.d("$this::init")
-    playground()
+
+    playgroundNullableKey()
+    playgroundNonNullKey()
   }
 
-  private fun playground() {
+  private fun playgroundNullableKey() {
     // Observe
-    stateFlow
-      .onEach { Napier.d("$this:: stateFlow=${it.contentToString()}") }
+    nullableStateFlow
+      .onEach { Napier.d("$this: NULLABLE_PARCELABLE_ARRAY_KEY nullableStateFlow=${it.contentToString()}") }
       .launchIn(viewModelScope)
 
     // Read
     savedStateHandle
-      .safe { it[PARCELABLE_ARRAY_KEY] }
-      .let { Napier.d("$this: saved=${it.contentToString()}") }
+      .safe { it[NULLABLE_PARCELABLE_ARRAY_KEY] }
+      .let { Napier.d("$this: NULLABLE_PARCELABLE_ARRAY_KEY saved=${it.contentToString()}") }
 
     // Write
     savedStateHandle
       .safe {
-        it[PARCELABLE_ARRAY_KEY] = arrayOf(
+        it[NULLABLE_PARCELABLE_ARRAY_KEY] = arrayOf(
           TestParcelable(System.currentTimeMillis()),
           null,
           TestParcelable(System.currentTimeMillis()),
         )
-        it[PARCELABLE_ARRAY_KEY]
+        it[NULLABLE_PARCELABLE_ARRAY_KEY]
       }
-      .let { Napier.d("$this: updated=${it.contentToString()}") }
+      .let { Napier.d("$this: NULLABLE_PARCELABLE_ARRAY_KEY updated=${it.contentToString()}") }
+  }
+
+  private fun playgroundNonNullKey() {
+    // Observe
+    nonNullStateFlow
+      .onEach { Napier.d("$this:: nonNullStateFlow=${it.contentToString()}") }
+      .launchIn(viewModelScope)
+
+    // Read
+    savedStateHandle
+      .safe { it[NON_NULL_PARCELABLE_ARRAY_KEY] }
+      .let { Napier.d("$this: NON_NULL_PARCELABLE_ARRAY_KEY saved=${it.contentToString()}") }
+
+    // Write
+    savedStateHandle
+      .safe {
+        it[NON_NULL_PARCELABLE_ARRAY_KEY] = arrayOf(
+          TestParcelable(System.currentTimeMillis()),
+          null,
+          TestParcelable(System.currentTimeMillis()),
+        )
+        it[NON_NULL_PARCELABLE_ARRAY_KEY]
+      }
+      .let { Napier.d("$this: NON_NULL_PARCELABLE_ARRAY_KEY updated=${it.contentToString()}") }
   }
 
   private companion object {
-    private val PARCELABLE_ARRAY_KEY = NullableSavedStateHandleKey.parcelableArray<TestParcelable>("parcelableArrayKey")
+    private val NULLABLE_PARCELABLE_ARRAY_KEY =
+      NullableSavedStateHandleKey.parcelableArray<TestParcelable>("NULLABLE_PARCELABLE_ARRAY_KEY")
+
+    private val NON_NULL_PARCELABLE_ARRAY_KEY = NonNullSavedStateHandleKey.parcelableArray(
+      key = "NON_NULL_PARCELABLE_ARRAY_KEY",
+      defaultValue = arrayOf(TestParcelable(1), null, TestParcelable(3)),
+    )
   }
 }
