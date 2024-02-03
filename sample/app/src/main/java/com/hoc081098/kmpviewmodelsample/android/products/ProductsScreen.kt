a145c367f8f538728f764096131c493d1050fbdf
hoc081098 <hoc081098@gmail.com>
2024-02-03 19:29:59 +0700

    sample: add ProductsScreen

--- sample/app/src/main/java/com/hoc081098/kmpviewmodelsample/android/products/ProductsScreen.kt
+++ sample/app/src/main/java/com/hoc081098/kmpviewmodelsample/android/products/ProductsScreen.kt
@@ -3,6 +3,7 @@
 package com.hoc081098.kmpviewmodelsample.android.products
 
 import android.widget.Toast
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
@@ -16,17 +17,25 @@ import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.withFrameMillis
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.datasource.CollectionPreviewParameterProvider
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.hoc081098.kmpviewmodelsample.ProductItemUi
 import com.hoc081098.kmpviewmodelsample.android.common.CollectWithLifecycleEffect
 import com.hoc081098.kmpviewmodelsample.android.common.EmptyProducts
 import com.hoc081098.kmpviewmodelsample.android.common.ErrorMessageAndRetryButton
 import com.hoc081098.kmpviewmodelsample.android.common.LoadingIndicator
+import com.hoc081098.kmpviewmodelsample.android.common.MyApplicationTheme
 import com.hoc081098.kmpviewmodelsample.android.common.OnLifecycleEventWithBuilder
 import com.hoc081098.kmpviewmodelsample.android.common.ProductItemsList
 import com.hoc081098.kmpviewmodelsample.products.ProductSingleEvent
 import com.hoc081098.kmpviewmodelsample.products.ProductsAction
+import com.hoc081098.kmpviewmodelsample.products.ProductsState
 import com.hoc081098.kmpviewmodelsample.products.ProductsViewModel
 import io.github.aakira.napier.Napier
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toImmutableList
 import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 
@@ -36,6 +45,8 @@ fun ProductsScreen(
   modifier: Modifier = Modifier,
   viewModel: ProductsViewModel = koinViewModel(),
 ) {
+  val dispatch: (ProductsAction) -> Unit = remember(viewModel) { viewModel::dispatch }
+
   LaunchedEffect(viewModel) {
     if (!viewModel.stateFlow.value.hasContent) {
       viewModel.dispatch(ProductsAction.Load)
@@ -72,6 +83,25 @@ fun ProductsScreen(
   }
   viewModel.eventFlow.CollectWithLifecycleEffect(collector = eventHandler)
 
+  ListContent(
+    modifier = modifier,
+    state = state,
+    lazyListState = lazyListState,
+    navigateToProductDetail = navigateToProductDetail,
+    onRetry = { dispatch(ProductsAction.Load) },
+    onRefresh = { dispatch(ProductsAction.Refresh) },
+  )
+}
+
+@Composable
+private fun ListContent(
+  state: ProductsState,
+  navigateToProductDetail: (Int) -> Unit,
+  onRetry: () -> Unit,
+  onRefresh: () -> Unit,
+  modifier: Modifier = Modifier,
+  lazyListState: LazyListState = rememberLazyListState(),
+) {
   Surface(modifier = modifier) {
     when {
       state.isLoading -> {
@@ -80,7 +110,7 @@ fun ProductsScreen(
 
       state.error != null -> {
         ErrorMessageAndRetryButton(
-          onRetry = { viewModel.dispatch(ProductsAction.Load) },
+          onRetry = onRetry,
           errorMessage = state.error?.message ?: "Unknown error",
         )
       }
@@ -94,7 +124,7 @@ fun ProductsScreen(
           products = state.products,
           pullRefreshState = rememberPullRefreshState(
             refreshing = state.isRefreshing,
-            onRefresh = { viewModel.dispatch(ProductsAction.Refresh) },
+            onRefresh = onRefresh,
           ),
           isRefreshing = state.isRefreshing,
           lazyListState = lazyListState,
@@ -104,3 +134,74 @@ fun ProductsScreen(
     }
   }
 }
+
+@Preview(showBackground = true)
+@Composable
+private fun ListContentPreview(
+  @PreviewParameter(ListContentParameterProvider::class) state: ProductsState,
+) {
+  MyApplicationTheme {
+    ListContent(
+      state = state,
+      navigateToProductDetail = {},
+      onRetry = {},
+      onRefresh = {},
+    )
+  }
+}
+
+private class ListContentParameterProvider : CollectionPreviewParameterProvider<ProductsState>(
+  run {
+    val products = List(10) {
+      ProductItemUi(
+        id = it,
+        title = "title $it",
+        price = it,
+        description = "description $it",
+        images = persistentListOf(),
+        creationAt = "2023-02-11T02:45:59.000Z",
+        updatedAt = "2023-02-11T14:54:14.000Z",
+        category = ProductItemUi.CategoryUi(
+          id = it,
+          name = "category name $it",
+          image = "image",
+          creationAt = "2023-02-11T02:45:59.000Z",
+          updatedAt = "2023-02-11T14:54:14.000Z",
+        ),
+      )
+    }.toImmutableList()
+
+    listOf(
+      ProductsState(
+        isLoading = true,
+        error = null,
+        products = persistentListOf(),
+        isRefreshing = false,
+      ),
+      ProductsState(
+        isLoading = false,
+        error = RuntimeException("Network error"),
+        products = persistentListOf(),
+        isRefreshing = false,
+      ),
+      ProductsState(
+        isLoading = false,
+        error = null,
+        products = persistentListOf(),
+        isRefreshing = false,
+      ),
+      ProductsState(
+        isLoading = false,
+        error = null,
+        products = products,
+        isRefreshing = false,
+      ),
+      ProductsState(
+        isLoading = false,
+        error = null,
+        products = products,
+        isRefreshing = true,
+      ),
+    )
+  },
+)
