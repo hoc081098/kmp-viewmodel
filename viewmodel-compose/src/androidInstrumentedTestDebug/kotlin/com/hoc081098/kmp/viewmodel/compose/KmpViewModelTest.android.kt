4ab3bc97adbcd05fd152df7beb61dcc9e79523bc
hoc081098 <hoc081098@gmail.com>
2024-02-15 13:47:21 +0700

    add tests

--- viewmodel-compose/src/androidInstrumentedTestDebug/kotlin/com/hoc081098/kmp/viewmodel/compose/KmpViewModelTest.android.kt
+++ viewmodel-compose/src/androidInstrumentedTestDebug/kotlin/com/hoc081098/kmp/viewmodel/compose/KmpViewModelTest.android.kt
@@ -1,6 +1,14 @@
 package com.hoc081098.kmp.viewmodel.compose
 
+import android.app.Dialog
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.platform.LocalSavedStateRegistryOwner
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.lifecycle.setViewTreeLifecycleOwner
+import androidx.savedstate.setViewTreeSavedStateRegistryOwner
 import com.hoc081098.kmp.viewmodel.CreationExtras
 import com.hoc081098.kmp.viewmodel.CreationExtrasKey
 import com.hoc081098.kmp.viewmodel.InternalKmpViewModelApi
@@ -11,8 +19,11 @@ import com.hoc081098.kmp.viewmodel.ViewModelStoreOwner
 import com.hoc081098.kmp.viewmodel.buildCreationExtras
 import com.hoc081098.kmp.viewmodel.edit
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 import kotlin.test.assertSame
+import kotlin.test.assertTrue
 import org.junit.Rule
 import org.junit.Test
 
@@ -34,6 +45,39 @@ private class FakeViewModelStoreOwner : ViewModelStoreOwner {
 class KmpViewModelTest {
   @get:Rule
   val composeTestRule = createComposeRule()
+
+  @Test
+  public fun nullViewModelStoreOwner() {
+    var owner: ViewModelStoreOwner? = null
+
+    composeTestRule.setContent {
+      val context = LocalContext.current
+      val lifecycleOwner = LocalLifecycleOwner.current
+      val savedStateRegistryOwner = LocalSavedStateRegistryOwner.current
+      DisposableEffect(context, lifecycleOwner, savedStateRegistryOwner) {
+        val dialog = Dialog(context).apply {
+          val composeView = ComposeView(context)
+          composeView.setContent {
+            // This should return null because no LocalViewModelStoreOwner was set
+            owner = LocalViewModelStoreOwner.current
+          }
+          setContentView(composeView)
+        }
+        dialog.show()
+        dialog.window?.decorView?.run {
+          // Specifically only set the LifecycleOwner and SavedStateRegistryOwner
+          setViewTreeLifecycleOwner(lifecycleOwner)
+          setViewTreeSavedStateRegistryOwner(savedStateRegistryOwner)
+        }
+
+        onDispose {
+          dialog.dismiss()
+        }
+      }
+    }
+
+    assertNull(owner)
+  }
 
   @Test
   fun viewModelCreatedViaFactory() {
@@ -154,5 +198,48 @@ class KmpViewModelTest {
       expected = "value",
       actual = createdInComposition1!!.extras[extrasKey],
     )
+  }
+
+  @OptIn(InternalKmpViewModelApi::class)
+  @Test
+  fun viewModelCreatedViaFactoryWithCustomOwner() {
+    val customOwner = FakeViewModelStoreOwner()
+    val owner = FakeViewModelStoreOwner()
+
+    var createdInComposition1: TestViewModel? = null
+    var createdInComposition2: TestViewModel? = null
+
+    composeTestRule.setContent {
+      ViewModelStoreOwnerProvider(owner) {
+        createdInComposition1 = kmpViewModel(viewModelStoreOwner = customOwner) {
+          TestViewModel(extras = this)
+        }
+        createdInComposition2 = kmpViewModel(viewModelStoreOwner = customOwner) {
+          TestViewModel(extras = this)
+        }
+      }
+    }
+
+    assertNotNull(createdInComposition1)
+    assertNotNull(createdInComposition2)
+    assertSame(createdInComposition1, createdInComposition2)
+
+    val expectedKey =
+      "androidx.lifecycle.ViewModelProvider.DefaultKey:com.hoc081098.kmp.viewmodel.compose.TestViewModel"
+    assertEquals(
+      expected = expectedKey,
+      actual = createdInComposition1!!.extras[VIEW_MODEL_KEY],
+    )
+
+    assertTrue {
+      customOwner.store
+        .keys()
+        .contains(expectedKey)
+    }
+    assertFalse {
+      owner.store
+        .keys()
+        .contains(expectedKey)
+    }
   }
 }
