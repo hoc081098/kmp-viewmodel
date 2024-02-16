7bb02f011b4167832543b4a7ad79a83e6667ea2c
hoc081098 <hoc081098@gmail.com>
2024-02-16 23:13:36 +0700

    fix gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,13 @@ import io.gitlab.arturbosch.detekt.DetektPlugin
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
+import org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockMismatchReport
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
@@ -53,13 +58,32 @@ dependencies {
   kover(project(":viewmodel-koin-compose"))
 }
 
-// Reference: https://github.com/drewhamilton/Poko/blob/72ec8d24cf48a74b3d1125c94f0e625ab956b93f/build.gradle.kts#L15-L22
+/**
+ * [Reference](https://github.com/square/okio/blob/55b7210fb3d52de07f4bc1122c5062e38df576d9/build.gradle.kts#L227-L248).
+ *
+ * Select a NodeJS version with WASI and WASM GC.
+ * https://github.com/Kotlin/kotlin-wasm-examples/blob/main/wasi-example/build.gradle.kts
+ */
 plugins.withType<NodeJsRootPlugin> {
   extensions.getByType<NodeJsRootExtension>().apply {
-    // WASM requires a canary Node.js version. This is the last v21 canary, and has both
-    // darwin-arm64 and darwin-x64 artifacts:
-    nodeVersion = "21.0.0-v8-canary20231024d0ddc81258"
-    nodeDownloadBaseUrl = "https://nodejs.org/download/v8-canary"
+    if (DefaultNativePlatform.getCurrentOperatingSystem().isWindows) {
+      // We're waiting for a Windows build of NodeJS that can do WASM GC + WASI.
+      nodeVersion = "21.4.0"
+    } else {
+      // Reference:
+      // https://github.com/drewhamilton/Poko/blob/72ec8d24cf48a74b3d1125c94f0e625ab956b93f/build.gradle.kts#L17-L19
+      // WASM requires a canary Node.js version. This is the last v21 canary, and has both
+      // darwin-arm64 and darwin-x64 artifacts:
+      nodeVersion = "21.0.0-v8-canary20231024d0ddc81258"
+      nodeDownloadBaseUrl = "https://nodejs.org/download/v8-canary"
+    }
+  }
+  // Suppress an error because yarn doesn't like our Node version string.
+  //   warning You are using Node "21.0.0-v8-canary20231024d0ddc81258" which is not supported and
+  //   may encounter bugs or unexpected behavior.
+  //   error karma@6.4.2: The engine "node" is incompatible with this module.
+  tasks.withType<KotlinNpmInstallTask>().all {
+    args += "--ignore-engines"
   }
 }
 
