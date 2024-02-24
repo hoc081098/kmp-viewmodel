a81ae65043a96e0c65f16f0a249ae517346e61d7
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2024-02-25 00:10:20 +0700

    maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots/")

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,7 @@ dependencyResolutionManagement {
     maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
     gradlePluginPortal()
     maven(url = "https://androidx.dev/storage/compose-compiler/repository/")
+    maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots/")
   }
 }
 
