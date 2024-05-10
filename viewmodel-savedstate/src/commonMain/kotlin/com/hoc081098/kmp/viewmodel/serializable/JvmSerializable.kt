8559ec969e1644a122cade7afef589de4ec724e4
hoc081098 <hoc081098@gmail.com>
2024-05-10 16:17:12 +0700

    done

--- viewmodel-savedstate/src/commonMain/kotlin/com/hoc081098/kmp/viewmodel/serializable/JvmSerializable.kt
+++ viewmodel-savedstate/src/commonMain/kotlin/com/hoc081098/kmp/viewmodel/serializable/JvmSerializable.kt
@@ -7,3 +7,10 @@ package com.hoc081098.kmp.viewmodel.serializable
  * On non-JVM platforms, this interface is a no-op and does not provide any actual serialization capabilities.
  */
 public expect interface JvmSerializable
+
+@Deprecated(
+  message = "JvmSerializable should be used instead to avoid confusion with KotlinX-Serialization @Serializable",
+  replaceWith = ReplaceWith("com.hoc081098.kmp.viewmodel.serializable.JvmSerializable"),
+  level = DeprecationLevel.WARNING,
+)
+public typealias Serializable = JvmSerializable
