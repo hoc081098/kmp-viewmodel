b373f5a54afb02de651ea873a5daeccec626a72a
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2024-05-21 10:42:13 +0700

    fix android

--- viewmodel/src/androidMain/kotlin/com/hoc081098/kmp/viewmodel/ViewModelFactory.android.kt
+++ viewmodel/src/androidMain/kotlin/com/hoc081098/kmp/viewmodel/ViewModelFactory.android.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModelProvider
 
 public fun <VM : ViewModel> ViewModelFactory<VM>.toAndroidX(): ViewModelProvider.Factory =
   object : ViewModelProvider.Factory {
-    override fun <T : androidx.lifecycle.ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+    override fun <T : androidx.lifecycle.ViewModel> create(modelClass: KClass<T>, extras: CreationExtras): T {
       @Suppress("UNCHECKED_CAST")
       return this@toAndroidX.create(extras) as T
     }
