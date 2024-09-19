aec51a86f3b432ac6521c90151df27c60d400f93
hoc081098 <hoc081098@gmail.com>
2024-09-19 18:06:43 +0700

    apiDump

--- sample/shared/build.gradle.kts
+++ sample/shared/build.gradle.kts
@@ -35,7 +35,7 @@ kotlin {
     summary = "Some description for the Shared Module"
     homepage = "Link to the Shared Module homepage"
     version = "1.0"
-    ios.deploymentTarget = "14.1"
+    ios.deploymentTarget = "15"
     podfile = project.file("../iosApp/Podfile")
     framework {
       baseName = "shared"
